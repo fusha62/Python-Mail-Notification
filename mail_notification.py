cf1a67f979e1df2c0e1a6beb18af5c8a25f73bba
Tomohiro Minagawa <fusha01@hotmail.com>
2012-02-24 23:33:01 +0900

    IMAPを使ったメールチェック部分はhttps://gist.github.com/1330542を参考に．プログラムの引数はargparseを使って変数に渡してみた．色々実験段階なコードだけど，とりあえずコミット．

---
+++ mail_notification.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Mail Notification for Ubuntu.')
+parser.add_argument('-s','--servername', nargs=1,
+		    metavar='<servername>',
+		    help='IMAP server address')
+parser.add_argument('-u','--username', nargs=1,
+		    metavar='<userID>',
+		    help='IMAP server address')
+
+args = parser.parse_args()
+servername = args.servername[0]
+username = args.username[0]
+
+print(servername)
+print(username)
+
+mail = imaplib.IMAP4_SSL(servername)
+mail.login(username,password)
+mail.list()
+mail.select("Inbox")
+status,maillist = mail.search(None,"(UNSEEN)")
+
+if status == "OK":
+        if maillist[0] is not '':
+            print "New Mail"
