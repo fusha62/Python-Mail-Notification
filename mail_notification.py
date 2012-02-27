f57991780d89342a0dd37bd11c505176ce24e41f
Tomohiro Minagawa <fusha01@hotmail.com>
2012-02-27 23:53:23 +0900

    一定時間間隔で，IMAPサーバーに新着メールを問い合わせるようにした．

--- mail_notification.py
+++ mail_notification.py
@@ -3,6 +3,7 @@
 
 import argparse
 import sys
+import time
 import getpass
 import imaplib
 
@@ -15,23 +16,30 @@ parser.add_argument('-u','--username', nargs=1,
 		    metavar='<userID>',
 		    help='your user ID',
 		    required=True)
+parser.add_argument('-t','--time', nargs=1, default=[60], type=int,
+		    metavar='<Sleep Time>',
+		    help='Sleep Time ( default = 60 sec )')
 
 args = parser.parse_args()
 servername = args.servername[0]
 username = args.username[0]
+set_time = args.time[0]
 password = getpass.getpass()
 
-print "IMAP Server : ",servername
-print "IMAP User Name : ",username
+print "IMAP Server        : ",servername
+print "IMAP User Name     : ",username
+print "Refresh Time [sec] : ",set_time
 
 print "====++++  New Mail Waiting  ++++===="
 
 mail = imaplib.IMAP4_SSL(servername)
 mail.login(username,password)
-mail.list()
-mail.select("Inbox")
-status,maillist = mail.search(None,"(UNSEEN)")
 
-if status == "OK":
-        if maillist[0] is not '':
-            print "New Mail"
+while True:
+	mail.list()
+	mail.select("Inbox")
+	status,maillist = mail.search(None,"(UNSEEN)")
+	if status == "OK":
+		if maillist[0] is not '':
+			print "New Mail"
+	time.sleep(set_time)
