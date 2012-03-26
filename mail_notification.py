885384483381e05fc9ca4e9463904a397946b846
tomohiro <tomohiro@EeePC-901X.(none)>
2012-03-26 18:49:39 +0900

    gnome-keyringを利用してパスワードを保存するようにした．

--- mail_notification.py
+++ mail_notification.py
@@ -8,7 +8,15 @@ import datetime
 import getpass
 import imaplib
 import pynotify
+import gnomekeyring as gk
 from os import path
+
+def ret_passwd_from_keyrings(keyring_name,servername):
+	item_keys = gk.list_item_ids_sync(keyring_name)
+	for key in item_keys:
+		item_info = gk.item_get_info_sync(keyring_name,key)
+		if item_info.get_display_name() == servername:
+			return item_info.get_secret()
 
 parser = argparse.ArgumentParser(description='Mail Notification for Ubuntu.')
 parser.add_argument('-s','--servername', nargs=1,
@@ -22,12 +30,20 @@ parser.add_argument('-u','--username', nargs=1,
 parser.add_argument('-t','--time', nargs=1, default=[60], type=int,
 		    metavar='<Sleep Time>',
 		    help='Sleep Time ( default = 60 sec )')
+parser.add_argument('-gk','--with-gnome-keyring', dest='gk',
+		    action="store_true", default=False,
+		    help="Using Gnome-Keyring (Save Password)")
 
 args = parser.parse_args()
 servername = args.servername[0]
 username = args.username[0]
 set_time = args.time[0]
-password = getpass.getpass()
+
+## Get Password ##
+if args.gk:
+	password = ret_passwd_from_keyrings('login',servername)
+else:
+	password = getpass.getpass()
 
 print "IMAP Server        : ",servername
 print "IMAP User Name     : ",username
